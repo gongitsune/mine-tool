ae19d0bae11ea73899ff9c6587334db855bfa43d
gongitsune <89068705+gongitsune@users.noreply.github.com>
2022-01-19 17:36:58 +0900

    Update

--- src/setup.py
+++ src/setup.py
@@ -2,6 +2,6 @@ from cx_Freeze import setup, Executable
 
 base = None
 
-exe = Executable(script="src/watch.py", base=base)
+exe = Executable(script="src/main.py", base=base)
 
 setup(name="mine_tool", version="0.2", description="converter", executables=[exe])
